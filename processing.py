464275651eb27efa395086e88ba3bf3ef205596e
marijanbeg <m.beg@soton.ac.uk>
2020-08-30 18:03:01 +0100

    Add sums.

--- processing.py
+++ processing.py
@@ -384,6 +384,39 @@ class Module:
         return Train(data=data, pattern=self.pattern,
                      xgm=self.xgm.train(index))
 
+    def sum_frames(self, frame_type, trains, njobs=40):
+        """Sums all individual frame values."""
+        # If train indices are not specified, all trains are processed.
+        if trains is None:
+            trains = range(self.ntrains)
+
+        # Function ran on an single thread.
+        def thread_func(chunk_number, job_trains):
+            accumulator = np.zeros((self.nframes(frame_type), len(job_trains)),
+                                   dtype='float64')
+
+            # We iterate through all trains.
+            for i in job_trains:
+                train = self.train(i)  # extract the train object
+                if train.valid:  # Train is valid if it contains image.data.
+                    accumulator[i, :] = np.sum(train[frame_type].data,
+                                               axis=(1, 2))
+
+            return chunk_number, accumulator.reshape(1, -1)
+
+        # Run jobs in parallel. Multiprocessing backend to preserve the order.
+        ranges = job_chunks(njobs, len(trains))  # distribute trains
+        res = joblib.Parallel(n_jobs=njobs, backend="multiprocessing")(
+            joblib.delayed(thread_func)(chunk_number, i)
+            for chunk_number, i in enumerate(ranges))
+
+        # Extract and sum results from individual jobs.
+        total_sum = np.concatenate(list(zip(*res))[0], axis=None)
+        total_number = np.concatenate(list(zip(*res))[1], axis=None)
+
+        # Compute average and "squeeze" to remove empty dimension.
+        return total_sum, total_number
+
     def average_frame(self, frame_type, trains=None, njobs=40):
         """This method computes the average of all frames through trains.
 
