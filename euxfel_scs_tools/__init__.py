8040433fe3003ec4cb39eb7e0304eb87a0656c3a
marijanbeg <m.beg@soton.ac.uk>
2020-08-31 11:26:14 +0100

    init

--- euxfel_scs_tools/__init__.py
+++ euxfel_scs_tools/__init__.py
@@ -1,1 +1,6 @@
-from .tst import add
+from .xgm import XGM
+from .container import Container
+from .train import Train
+from .module import Module
+from .read import read
+from .reduction import reduction_std, reduction_norm
